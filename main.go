ff0c4f8771f8f57543963a12a73827418715c174
votebmyla <votebmyla@gmail.com>
2022-06-16 11:22:25 +0500

    feat: add item

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -17,17 +19,23 @@ func main() {
 }
 
 func Perform(args Arguments, writer io.Writer) error {
-
+	// fmt.Println(args)
+	if len(args["fileName"]) == 0 {
+		return errFilename
+	}
 	switch args["operation"] {
 	case "add":
-		fmt.Println("this is add operation")
-	case "list":
-		if len(args["fileName"]) == 0 {
-			return errors.New("-fileName flag has to be specified")
+		if len(args["item"]) == 0 {
+			return errItem
 		}
+		addItem(args, writer)
+	case "list":
 		listItems(args, writer)
 	case "findById":
-		findById(args)
+		if len(args["id"]) == 0 {
+			return errId
+		}
+		findById(args, writer)
 	case "remove":
 		fmt.Println("this is remove operation")
 	default:
@@ -44,7 +52,7 @@ type Arguments map[string]string
 
 func parseArgs() Arguments {
 	userOperation := flag.String("operation", "", "invalid operation")
-	userId := flag.String("id", "-1", "invalid id")
+	userId := flag.String("id", "", "invalid id")
 	userItem := flag.String("item", "", "invalid item")
 	userFilename := flag.String("fileName", "", "invalid filename")
 
@@ -61,7 +69,7 @@ func parseArgs() Arguments {
 
 func listItems(userArgs Arguments, writer io.Writer) error {
 	// fmt.Println(userArgs)
-	file, err := os.OpenFile(userArgs["fileName"], os.O_RDWR, 0755)
+	file, err := os.OpenFile(userArgs["fileName"], os.O_RDONLY, 0444)
 	if err != nil {
 		return err
 	}
@@ -78,18 +86,89 @@ func listItems(userArgs Arguments, writer io.Writer) error {
 	}
 }
 
-func findById(args Arguments) error {
-
-	// file, err := os.OpenFile("")
-	// for i, v := range args {
-	// 	fmt.Println(args[i], v)
-	// }
+func addItem(args Arguments, writer io.Writer) error {
+	jsonFile, err := os.Open(args["fileName"])
+	if err != nil {
+		newJsonFile, err := os.Create(args["fileName"])
+		if err != nil {
+			return err
+		}
+		defer newJsonFile.Close()
+		var userDataInput User
+		err = json.Unmarshal([]byte(args["item"]), &userDataInput)
+		if err != nil {
+			return err
+		}
+		var userDataInputArr []User
+		userDataInputArr = append(userDataInputArr, userDataInput)
+		userDataInputArrByte, err := json.Marshal(userDataInputArr)
+		if err != nil {
+			return err
+		}
+		err = ioutil.WriteFile(args["fileName"], userDataInputArrByte, 0644)
+		if err != nil {
+			return err
+		}
+	}
+	defer jsonFile.Close()
+	jsonFileByte, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return err
+	}
+	var jsonFileArr []User
+	err = json.Unmarshal(jsonFileByte, &jsonFileArr)
+	if err != nil {
+		return err
+	}
+	//
+	for _, v := range jsonFileArr {
+		var itemData User
+		err := json.Unmarshal([]byte(args["item"]), &itemData)
+		if err != nil {
+			return err
+		}
+		if itemData.Id == v.Id {
+			s := fmt.Sprintf("Item with id %s already exists", itemData.Id)
+			writer.Write([]byte(s))
+		}
+	}
+	//
 
 	return nil
 }
 
-// func checkOperationFlag(args Arguments) error {
-// 	switch args["operation"]{
-// 		case ""
-// 	}
-// }
+func findById(args Arguments, writer io.Writer) error {
+	jsonFile, err := os.OpenFile(args["fileName"], os.O_RDONLY, 0444)
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return err
+	}
+	var users Users
+	json.Unmarshal(byteValue, &users)
+	for _, v := range users {
+		if v.Id == args["id"] {
+			data, err := json.Marshal(v)
+			if err != nil {
+				return err
+			}
+			writer.Write(data)
+		}
+	}
+	return nil
+}
+
+var errFilename error = errors.New("-fileName flag has to be specified")
+var errId error = errors.New("-id flag has to be specified")
+var errItem error = errors.New("-item flag has to be specified")
+
+type User struct {
+	Id    string `json:"id"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
+
+type Users []User
